60071615ea6085e846a713aa993920b568bcc32a
Michael Rüttgers <mr@westwerk.ac>
2016-03-12 17:56:42 +0100

    Removed all obsolete files

--- clean_state_by_empty_name.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Removes node from state-file
-
-BASEDIR=/opt/freifunk
-
-jq "[.[]|select(.name|length > 0)]" $BASEDIR/ffmap-backend-legacy/state.json > $BASEDIR/ffmap-backend-legacy/state.json.tmp
-mv $BASEDIR/ffmap-backend-legacy/state.json.tmp $BASEDIR/ffmap-backend-legacy/state.json
-
